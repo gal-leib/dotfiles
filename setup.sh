6508ba4bc729d048cab63aca76824b2c6dcc86b9
Gal Leibovici <gleibovi@redhat.com>
2012-12-02 16:40:12 +0200

    Added links

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #set up Vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
+#make links
+setopt EXTENDED_GLOB
+for rcfile in dotfiles/^setup.sh(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
